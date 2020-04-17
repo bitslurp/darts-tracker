b9b0786ca8aa922151546efeb8a9ef494645ea45
Will Snowdon <will.snowdon@hotmail.co.uk>
2020-04-17 13:42:13 +0100

    Adds initial generated app

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App: FunctionComponent = () => {
+  return (
+    <SafeAreaView style={{backgroundColor: 'white'}}>
+      <Text>Darts TRACKER</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
